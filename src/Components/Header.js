ec2c8c3a0a897ad9b89ca306e8749466247ebaa2
BuildTools <unconfigured@null.spigotmc.org>
2022-02-25 18:06:14 +0530

    made nice header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,18 +4,31 @@ import { Link } from 'react-router-dom';
 import '../CSS/Header.css';
 
 class Header extends Component {
-    state = {  } 
+    state = {  }
+
+    showMenu = (event) => {
+        document.getElementById("collapsible-menu").style.display = "block";
+        document.getElementById("show-menu").style.display = "none";
+    };
+
     render() { 
         return (
-            <div className="header">
+            <>
+            <div id="show-menu" onClick={this.showMenu}>
+                <div className="hmbg"></div>
+                <div className="hmbg"></div>
+                <div className="hmbg"></div>
+            </div>
+            <div id="collapsible-menu" className="header">
                 <nav className="navbar container">
-                    <div className="row row-cols-1 row-cols-sm-3 justify-content-end">
+                    <div className="row justify-content-end">
                         <div className="col"><Link className="link" to="/">Home</Link></div>
                         <div className="col"><Link className="link" to="/login">Login</Link></div>
                         <div className="col"><Link className="link" to="/profile">Profile</Link></div>
                     </div>
                 </nav>
             </div>
+            </>
         );
     }
 }
