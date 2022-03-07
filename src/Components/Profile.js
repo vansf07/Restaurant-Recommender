476088464f249d008c32edeff8ab42e70cbd3460
Ranjana Kambhammettu <ranjanak207@gmail.com>
2022-03-07 18:59:20 +0400

    Added profile name and picture

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styles from '../CSS/Profile.module.css';
-
+import profilepic from '../assets/jayant-dassz-u08NEYUYvig-unsplash.jpg';
 import Footer from './Footer';
 import Header from './Header';
 class Profile extends Component {
@@ -10,6 +10,8 @@ class Profile extends Component {
             <div className={styles.body}>
                 <Header />
                 <h1>Profile</h1>
+                <img src={profilepic}></img>
+                <h2>Rani Kumar</h2>
                 <Footer />
             </div>
         );
