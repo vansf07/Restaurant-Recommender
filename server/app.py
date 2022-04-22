7ee76dd3f704ae65329a8c0128f214bd03bbbc9a
famousfive1 <tejaswihegde1@gmail.com>
2022-04-23 00:01:35 +0530

    made page reloads work

--- server/app.py
+++ server/app.py
@@ -1,7 +1,12 @@
+import os
 from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_url_path='', static_folder='../build/')
+app = Flask(__name__, static_folder='../build/')
 
-@app.route('/')
-def send_index():
-    return send_from_directory('../build/', 'index.html')
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
